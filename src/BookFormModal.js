0d39c68171d74e78c737b807b6115dd9e538933f
Elizabeth <elisha.elizabeth@gmail.com>
2022-05-24 20:37:05 -0700

    Updates to app for lab12

---
+++ src/BookFormModal.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
+
+class BookFormModal extends React.Component {
+
+
+handleBookSubmit = (e) => {
+  e.preventDefault();
+  let newBook = {
+    title: e.target.title.value,
+    description: e.target.description.value,
+    recommended: e.target.recommended.checked,
+  }
+  this.props.postBooks(newBook);
+}
+
+  render() {
+
+    return (
+      <>
+        <Modal
+          show={this.props.isModalDisplaying}
+          onHide={this.props.closeModal}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Add Book</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleBookSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Label>Book Title</Form.Label>
+                <Form.Control type="text" id="title"/>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Description of Book</Form.Label>
+                <Form.Control type="text" id="description"/>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Check
+                  type="checkbox"
+                  id="recommended"
+                  label="Do you recommend the book?"
+                />
+              </Form.Group>
+              <Button 
+                variant="primary" 
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.closeModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    )
+  }
+}
+
+export default BookFormModal
